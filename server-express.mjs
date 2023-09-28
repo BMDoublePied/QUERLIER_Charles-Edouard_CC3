d1758fa75372c22db305720197dbc3298663e742
BMDoublePied <charlesedouard.querlier@gmail.com>
2023-09-28 20:12:40 +1100

    Gestion d’erreurs

--- server-express.mjs
+++ server-express.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import createError from "http-errors";
 
 const host = "localhost";
 const port = 8000;
@@ -8,17 +9,20 @@ const app = express();
 
 if (app.get("env") === "development") {
   app.use(morgan("dev"));
-}
+};
 
 app.set("view engine", "ejs");
 
 app.use(express.static("static"));
 
 app.get("/random/:nb", async function (request, response, next) {
-  const length = request.params.nb;
+  const length = Number.parseInt(request.params.nb, 10);
+  if (Number.isNaN(length)) {
+    return next(createError(400, "paramètre invalide : nb doit être un nombre"));
+  }
   const numbers = Array.from({ length }).map(() => Math.floor(100 * Math.random()));
   const welcome = "Bienvenu dans le site des nombres aléatoires";
-  
+
   response.render("random", { numbers, welcome });
 });
 
